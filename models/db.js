925f894307c8f90b01f625df72474a4e425dbdca
Yu Guo <y.guo38@student.unimelb.edu.au>
2018-03-28 10:02:56 +1100

    delete faker

--- models/db.js
+++ models/db.js
@@ -1,22 +1,29 @@
-var faker = require('faker');
+// var faker = require('faker');
+
+// var col = faker.database.column();
+// var type = faker.database.type();
+
+// var name = faker.Name.findName();
+// var address = faker.Address.city();
+// var email = faker.Internet.email();
 
 var numUsers = 10;
 // var users[];
 
 users = new Array(numUsers);
 
-for (i = 0; i < numUsers; i++) {
-    var name = faker.name.findName();
-    var address = faker.address.city();
-    var email = faker.internet.email();
-    users[i] = {
-
-        "id": i,
-        "name": name,
-        "address": address,
-        "email": email
-
-    }
-};
+// for (i = 0; i < numUsers; i++) {
+//     var name = faker.name.findName();
+//     var address = faker.address.city();
+//     var email = faker.internet.email();
+//     users[i] = {
+//
+//         "id": i,
+//         "name": name,
+//         "address": address,
+//         "email": email
+//
+//     }
+// };
 
 module.exports = users;

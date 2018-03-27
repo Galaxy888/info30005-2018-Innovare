8f67d07ec8f381b917aa4a85f148e157703fc415
Yu Guo <y.guo38@student.unimelb.edu.au>
2018-03-28 09:54:54 +1100

    try again with faker

--- models/db.js
+++ models/db.js
@@ -1,22 +1,22 @@
-// var faker = require('faker');
+var faker = require('faker');
 
 var numUsers = 10;
 // var users[];
 
 users = new Array(numUsers);
 
-// for (i = 0; i < numUsers; i++) {
-//     var name = faker.name.findName();
-//     var address = faker.address.city();
-//     var email = faker.internet.email();
-//     users[i] = {
-//
-//         "id": i,
-//         "name": name,
-//         "address": address,
-//         "email": email
-//
-//     }
-// };
+for (i = 0; i < numUsers; i++) {
+    var name = faker.name.findName();
+    var address = faker.address.city();
+    var email = faker.internet.email();
+    users[i] = {
+
+        "id": i,
+        "name": name,
+        "address": address,
+        "email": email
+
+    }
+};
 
 module.exports = users;

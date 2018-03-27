3af0db7bdc8603544729ebf4d5ba5543b95573b8
Admin <stanner1@student.unimelb.edu.au>
2018-03-27 19:37:54 +1100

    Uploading new index page, with stylesheet

--- W4/app.js
+++ W4/app.js
@@ -8,9 +8,9 @@ app.use(router);
 
 app.set('view engine','ejs');
 
-
 const PORT = process.env.PORT||3000;
 
-app.listen(3000,function(){
-    console.log('Express serving at port 3000')
+app.listen(PORT, function(){
+    console.log('Express listening on port ${PORT}');
 });
+

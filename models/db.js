ae4cbdeb4c301b8c132750a9d02e33e4fb89f0ab
Yu Guo <y.guo38@student.unimelb.edu.au>
2018-03-28 09:50:10 +1100

    comment out faker

--- models/db.js
+++ models/db.js
@@ -1,29 +1,22 @@
-var faker = require('faker');
-
-// var col = faker.database.column();
-// var type = faker.database.type();
-
-// var name = faker.Name.findName();
-// var address = faker.Address.city();
-// var email = faker.Internet.email();
+// var faker = require('faker');
 
 var numUsers = 10;
 // var users[];
 
 users = new Array(numUsers);
 
-for (i = 0; i < numUsers; i++) {
-    var name = faker.name.findName();
-    var address = faker.address.city();
-    var email = faker.internet.email();
-    users[i] = {
-
-        "id": i,
-        "name": name,
-        "address": address,
-        "email": email
-
-    }
-};
+// for (i = 0; i < numUsers; i++) {
+//     var name = faker.name.findName();
+//     var address = faker.address.city();
+//     var email = faker.internet.email();
+//     users[i] = {
+//
+//         "id": i,
+//         "name": name,
+//         "address": address,
+//         "email": email
+//
+//     }
+// };
 
 module.exports = users;

9127e51cc3a065135e362e3d97cedf3df1a46990
Yu Guo <y.guo38@student.unimelb.edu.au>
2018-04-22 20:35:55 +1000

    move css and img in js

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -10,25 +10,25 @@ module.exports.fetchMainPage =
 module.exports.fetchMainPageCSS =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/index.css'));
+        res.sendFile(path.join(__dirname, '..', '/public/css/index.css'));
     };
 
 module.exports.fetchMainPageLogo =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/live_school.png'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/live_school.png'));
     };
 
 module.exports.fetchMainPageLogo2 =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/innovare_logo.png'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/innovare_logo.png'));
     };
 
 module.exports.fetchMainPageBg =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/school_above.jpg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/school_above.jpg'));
     };
 
 module.exports.fetchAllUsers =
@@ -60,13 +60,13 @@ module.exports.fetchSignUP_teacher =
 module.exports.fetchSignUpCSS =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/signup.css'));
+        res.sendFile(path.join(__dirname, '..', '/public/css/signup.css'));
     };
 
 module.exports.fetchSignUpBg =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/srilanka_school.jpg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/srilanka_school.jpg'));
     };
 
 
@@ -86,55 +86,55 @@ module.exports.fetchSchool_profile =
 module.exports.fetchProfileCSS =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/profile.css'));
+        res.sendFile(path.join(__dirname, '..', '/public/css/profile.css'));
     };
 
 module.exports.fetchProfile_dashboard =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/dashboard.svg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/dashboard.svg'));
     };
 
 module.exports.fetchProfile_setting =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/setting.svg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/setting.svg'));
     };
 
 module.exports.fetchProfile_school =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/school.svg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/school.svg'));
     };
 
 module.exports.fetchProfile_teacher =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/teacher.svg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/teacher.svg'));
     };
 
 
 module.exports.fetchProfile_timetable =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/timetable.svg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/timetable.svg'));
     };
 
 module.exports.fetchProfile_signOut =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/Sign out.svg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/Sign out.svg'));
     };
 
 module.exports.fetchProfile_martin =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/martin_profile.jpg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/martin_profile.jpg'));
     };
 
 module.exports.fetchProfileBg =
     function(req,res){
         var path = require("path");
-        res.sendFile(path.join(__dirname, '..', '/views/sri_lanka_jungle.jpg'));
+        res.sendFile(path.join(__dirname, '..', '/public/img/sri_lanka_jungle.jpg'));
     };
 

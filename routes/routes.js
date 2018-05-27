3531c2b7ab78b8e40e709dd9cd2a8f021b69b037
Yu Guo <y.guo38@student.unimelb.edu.au>
2018-05-28 01:56:13 +1000

    remvoe unused code in js and dont show error

--- routes/routes.js
+++ routes/routes.js
@@ -40,25 +40,4 @@ router.get('/teacher_profile', controllers.fetchTeacher_profile);
 router.get('/school_profile', controllers.fetchSchool_profile);
 
 
-
-//add teacher (OBSOLETE - JUST HIRE, NOW)
-// router.post('/add_teacher',controllers.schoolAddTeacher);
-
-//delete teacher
-// router.post('/delete_teacher',controllers.schoolDeleteTeacher);
-
-// router.post('/signup_success',controllers.createTeacher);
-// router.post('/signup_success',controllers.createSchool);
-// router.get('/signup_success', controllers.fetchSignup_success);
-
-// Create new teacher
-// router.post('/sign_teacher_up',controllers.createTeacher);
-
-
-// Find all teacher
-// router.get('/signup_teacher',controllers.findAllTeachers);
-
-// Find one teacher by id
-// router.get('/signup_teacher/:id',controllers.findOneTeacher);
-
 module.exports = router;

57aec5c05ed8a50e68a1caf09dff73e83a6b9ecf
Yu Guo <y.guo38@student.unimelb.edu.au>
2018-05-27 16:21:21 +1000

    Now can edit teacher sunject and timetable

--- routes/routes.js
+++ routes/routes.js
@@ -18,8 +18,10 @@ router.get('/signup_teacher', controllers.fetchSignUP_teacher);
 
 
 //Update db
-router.post('/update_teacher',controllers.updateTeacherAvailabilities);
+// router.post('/update_teacher',controllers.updateTeacherAvailabilities);
 router.post('/edit_teacher_profile',controllers.updateTeacherProfile);
+router.post('/edit_teacher_subject',controllers.updateTeacherSubject);
+router.post('/edit_teacher_timetable',controllers.updateTeacherTimetable);
 router.post('/edit_school_profile',controllers.updateSchoolProfile);
 
 
